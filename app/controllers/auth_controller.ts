84b3677e81853526c68d081d9160fb0945561897
koirherianto <koir.harianto@gmail.com>
2025-01-01 18:11:44 +0800

    landing page

---
+++ app/controllers/auth_controller.ts
@@ -0,0 +1,45 @@
+import User from '#models/user'
+import { loginValidator, registerValidator } from '#validators/auth'
+import type { HttpContext } from '@adonisjs/core/http'
+
+import hash from '@adonisjs/core/services/hash'
+
+export default class AuthController {
+  async registerPage({ view }: HttpContext) {
+    return view.render('auth/register')
+  }
+
+  async register({ request, response, auth }: HttpContext) {
+    const data = await request.validateUsing(registerValidator)
+
+    const user = await User.create(data)
+
+    await auth.use('web').login(user)
+
+    response.redirect('/')
+  }
+
+  async loginpage({ view }: HttpContext) {
+    return view.render('auth/login2')
+  }
+
+  async login({ session, request, response, auth }: HttpContext) {
+    const { email, password } = await request.validateUsing(loginValidator)
+
+    const user = await User.findBy('email', email)
+    const isVerify = await hash.verify(user!.password, password)
+
+    if (isVerify && user) {
+      await auth.use('web').login(user)
+      response.redirect('/u/projects')
+    } else {
+      session.flash('inputErrorsBag', { email: ['Email or password is wrong'] })
+      response.redirect().back()
+    }
+  }
+
+  async logout({ response, auth }: HttpContext) {
+    await auth.use('web').logout()
+    return response.status(200).json({ success: true })
+  }
+}
--- start/routes.ts
+++ start/routes.ts
@@ -1,11 +1,19 @@
-/*
-|--------------------------------------------------------------------------
-| Routes file
-|--------------------------------------------------------------------------
-|
-| The routes file is used for defining the HTTP routes.
-|
-*/
+/* eslint-disable prettier/prettier */
+import { middleware } from './kernel.js'
+
+const LandingController = () => import('#controllers/landing_controller')
+const AuthController = () => import('#controllers/auth_controller')
 
 import router from '@adonisjs/core/services/router'
-router.on('/').renderInertia('home')
+// router.on('/').renderInertia('home')
+
+router.get('/', [LandingController, 'index'])
+
+
+router.group(() => {
+    router.get('/register', [AuthController, 'registerPage']).as('register')
+    router.post('/register', [AuthController, 'register']).as('register.store')
+
+    router.get('/login', [AuthController, 'loginpage']).as('login')
+    router.post('/login', [AuthController, 'login']).as('login.store')
+}).middleware(middleware.guest())
