05633e2a54f4cbdadc8c9dee21a7922033fa424e
koirherianto <koir.harianto@gmail.com>
2025-01-01 21:47:30 +0800

    landing update

--- app/controllers/landing_controller.ts
+++ app/controllers/landing_controller.ts
@@ -2,9 +2,7 @@ import type { HttpContext } from '@adonisjs/core/http'
 
 export default class LandingController {
   async index({ view, auth }: HttpContext) {
-    // return inertia.render('home', { version: 6 })
     await auth.check()
-
     return view.render('landing')
   }
 }
