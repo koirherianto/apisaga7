e4ec1dd8543747d473733b98ae2b331579b22364
koirherianto <koir.harianto@gmail.com>
2025-01-03 13:23:33 +0800

    dashboard routing

---
+++ app/controllers/dashboard_controller.ts
@@ -0,0 +1,41 @@
+import type { HttpContext } from '@adonisjs/core/http'
+
+export default class DashboardController {
+  /**
+   * Display a list of resource
+   */
+  async index({ view, auth }: HttpContext) {
+    await auth.check()
+    return view.render('dashboard/index')
+  }
+
+  /**
+   * Display form to create a new record
+   */
+  async create({}: HttpContext) {}
+
+  /**
+   * Handle form submission for the create action
+   */
+  async store({ request }: HttpContext) {}
+
+  /**
+   * Show individual record
+   */
+  async show({ params }: HttpContext) {}
+
+  /**
+   * Edit individual record
+   */
+  async edit({ params }: HttpContext) {}
+
+  /**
+   * Handle form submission for the edit action
+   */
+  async update({}: HttpContext) {}
+
+  /**
+   * Delete record
+   */
+  async destroy({ params }: HttpContext) {}
+}
--- start/routes.ts
+++ start/routes.ts
@@ -3,6 +3,7 @@ import { middleware } from './kernel.js'
 
 const LandingController = () => import('#controllers/landing_controller')
 const AuthController = () => import('#controllers/auth_controller')
+const DashboardController = () => import('#controllers/dashboard_controller')
 
 import router from '@adonisjs/core/services/router'
 // router.on('/').renderInertia('home')
@@ -19,6 +20,31 @@ router.group(() => {
 }).middleware(middleware.guest())
 
 
+
 router.group(() => {
     router.delete('/logout', [AuthController, 'logout']) // no csrf
+
+    router.group(() => {
+        // untuk user bisa mengedit data diri
+        // router.get('/profile', [ProfilesController, 'profileView']).as('profile')
+        // router.put('/profile', [ProfilesController, 'profileUpdate']).as('profile.update')
+
+        // untuk user bisa mengorganisir project nya sendiri
+        router.resource('dashboard', DashboardController).as('dashboard')
+
+    }).prefix('u')
+
+    // jika url tidak lengkap arahkan page default
+    router.group(() => {
+        // router.get(':project', [RedirectController, 'project']).as('redirect.project')
+        // router.get(':project/:version', [RedirectController, 'version']).as('redirect.version')
+        // router.get(':project/:version/:topbar', [RedirectController, 'topbar']).as('redirect.topbar')
+    })
+
+    // :project/:version/:topbar/:page/editor
+    // router.get(':project/:version/:topbar/:page/editor', [EditorController, 'index']).as('editor.index')
+    
 }).middleware(middleware.auth())
+
+// default page
+// router.get(':project/:version/:topbar/:page', [PageController, 'index']).as('page.index')
