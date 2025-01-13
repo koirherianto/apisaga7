9213b5db24a71373fb48065f350b3a1d01a3730c
koirherianto <koir.harianto@gmail.com>
2025-01-13 17:42:59 +0800

    tes alpine

--- app/controllers/landing_controller.ts
+++ app/controllers/landing_controller.ts
@@ -3,6 +3,6 @@ import type { HttpContext } from '@adonisjs/core/http'
 export default class LandingController {
   async index({ view, auth }: HttpContext) {
     await auth.check()
-    return view.render('landing')
+    return view.render('tes')
   }
 }
