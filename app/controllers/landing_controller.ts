8ed5328af586435c6e13cec0eab9854f9df8dd29
koirherianto <koir.harianto@gmail.com>
2025-01-14 14:00:40 +0800

    update landing page view rendering

--- app/controllers/landing_controller.ts
+++ app/controllers/landing_controller.ts
@@ -3,6 +3,6 @@ import type { HttpContext } from '@adonisjs/core/http'
 export default class LandingController {
   async index({ view, auth }: HttpContext) {
     await auth.check()
-    return view.render('tes')
+    return view.render('landing')
   }
 }
